3fb4766015194901c7d549145ca0019ea8f72311
Aryan <swapnil.kamble02@gmail.com>
2017-06-08 17:44:49 +0530

    Create childComponent.js

--- childComponent.js
+++ childComponent.js
@@ -21,13 +21,17 @@
                 myData:'<',
                 //Two Way
                 myTwoWay:'=',
-                //Input as String
-                myName:'@'
+                //Input as string
+                myName:'@',
+                //example on Changes fired from parent value is changed from parent scope then it 
+                //will reflect in child component 
+                primitiveValue:'<'
             },
         });
 
     //  ChildController.$inject = ['dependency1'];
-    function ChildController() {
+    ChildController.$inject=['$element'];
+    function ChildController($element) {
         var $ctrl = this;
         ////////////////
         $ctrl.parentAnotherClick = function () {
@@ -35,8 +39,34 @@
                 EmployeeName: 'Kamble'
             });
         };
-        $ctrl.$onInit = function () {  };
-        $ctrl.$onChanges = function (changesObj) { };
-        $ctrl.$onDestroy = function () { };
+        $ctrl.$onInit = function () { 
+            var btnElement= angular.element(document.querySelector('#btnDestory'));
+            console.log(btnElement);
+            btnElement.on('click',function(){
+                alert('Destory');
+            });
+         };
+        $ctrl.$onChanges = function (changesObj) {
+            console.log(changesObj);
+
+         };
+        $ctrl.$onDestroy = function () {
+            // Destroy all custom events or bindings when the component scope is destroyed.
+            console.log('this is not the right way...');
+             var btnElement= angular.element(document.querySelector('#btnDestory'));
+             btnElement.unbind('click');
+         };
+
+         $ctrl.$doCheck =function(){
+             console.log('do check');
+             console.log($ctrl.primitiveValue);
+         };  
+
+         ///Modify the DOM then use the Post Link 
+         $ctrl.$postLink=function(){
+             console.log($element);
+         };
+         
+
     }
 })();        
